20aec0a640aa242392f1f0d84385657782bcbe7f
hak363 <hakimhakimov711@gmail.com>
2025-11-27 19:46:44 +0500

    Update Source1.cpp

--- Project34/Source1.cpp
+++ Project34/Source1.cpp
@@ -1,30 +1,34 @@
 #include <iostream>
 
-int main() {
+int main() 
+{
+    SetConsoleCP(1251); 
+    SetConsoleOutputCP(1251);
+    
     double distance, fuel_100;
     double price1, price2, price3;
 
-    std::cout << "Введите расстояние поездки (км): ";
+    std::cout << "Г‚ГўДєГ¤ДЌЕ€Дє Д‘Е•Е„Е„Е€Г®Л™Г­ДЌДє ДЏГ®ДєГ§Г¤Д™ДЌ (Д™Д›): ";
     std::cin >> distance;
 
-    std::cout << "Введите расход бензина на 100 км: ";
+    std::cout << "Г‚ГўДєГ¤ДЌЕ€Дє Д‘Е•Е„Е‘Г®Г¤ ГЎДєГ­Г§ДЌГ­Е• Г­Е• 100 Д™Д›: ";
     std::cin >> fuel_100;
 
-    std::cout << "Введите цену бензина A: ";
+    std::cout << "Г‚ГўДєГ¤ДЌЕ€Дє Г¶ДєГ­Гі ГЎДєГ­Г§ДЌГ­Е• A: ";
     std::cin >> price1;
 
-    std::cout << "Введите цену бензина B: ";
+    std::cout << "Г‚ГўДєГ¤ДЌЕ€Дє Г¶ДєГ­Гі ГЎДєГ­Г§ДЌГ­Е• B: ";
     std::cin >> price2;
 
-    std::cout << "Введите цену бензина C: ";
+    std::cout << "Г‚ГўДєГ¤ДЌЕ€Дє Г¶ДєГ­Гі ГЎДєГ­Г§ДЌГ­Е• C: ";
     std::cin >> price3;
 
-    double fuelNeeded = distance / 100 * fuel_100; // сколько потребуется топлива
+    double fuelNeeded = distance / 100 * fuel_100; // Е„Д™Г®Г«ГјД™Г® ДЏГ®Е€Д‘ДєГЎГіДєЕ€Е„Л™ Е€Г®ДЏГ«ДЌГўЕ•
 
-    std::cout << "\n--- Стоимость поездки ---\n";
-    std::cout << "Бензин A: " << fuelNeeded * price1 << " грн\n";
-    std::cout << "Бензин B: " << fuelNeeded * price2 << " грн\n";
-    std::cout << "Бензин C: " << fuelNeeded * price3 << " грн\n";
+    std::cout << "\n--- ЕѓЕ€Г®ДЌД›Г®Е„Е€Гј ДЏГ®ДєГ§Г¤Д™ДЌ ---\n";
+    std::cout << "ГЃДєГ­Г§ДЌГ­ A: " << fuelNeeded * price1 << " ДѓД‘Г­\n";
+    std::cout << "ГЃДєГ­Г§ДЌГ­ B: " << fuelNeeded * price2 << " ДѓД‘Г­\n";
+    std::cout << "ГЃДєГ­Г§ДЌГ­ C: " << fuelNeeded * price3 << " ДѓД‘Г­\n";
 
     return 0;
 }
